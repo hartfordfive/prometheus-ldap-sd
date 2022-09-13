8452bf75259ea11cc39abdbc52f45c1bfeb0a37a
Alain Lefebvre <hartfordfive@users.noreply.github.com>
2022-09-13 15:42:00 -0400

    Version 0.4.0 (#6)
    * Adding changes for v0.4.0

--- store/errors.go
+++ store/errors.go
@@ -1,37 +1,45 @@
 package store
 
-/*
-	RESULT_INVALID_QUERY
-	RESULT_MAX_RECONNECTS
-	RESULT_CACHE_ERROR
-	RESULT_CACHE_UPDATE_FAIL
-	RESULT_CACHE_FETCH_FAIL
-*/
+import "fmt"
 
+// LdapStoreErrorInvalidTargetGroup is an error relating to an invalid target group name
+type LdapStoreErrorInvalidTargetGroup struct {
+	TargetGroup string
+}
+
+func (e *LdapStoreErrorInvalidTargetGroup) Error() string {
+	return fmt.Sprintf("Invalid or empty target group specified: %s", e.TargetGroup)
+}
+
+// LdapStoreErrorInvalidQuery is an error relating to an invalide LDAP query
 type LdapStoreErrorInvalidQuery struct{}
 
 func (e *LdapStoreErrorInvalidQuery) Error() string {
 	return "Invalid LDAP query"
 }
 
+// LdapStoreErrorMaxReconnects is an error relating to hitting the maximum reconneciton attempts
 type LdapStoreErrorMaxReconnects struct{}
 
 func (e *LdapStoreErrorMaxReconnects) Error() string {
 	return "Maximum reconnection attempts reached"
 }
 
+// LdapStoreErrorCache is an error relating to a general caching error
 type LdapStoreErrorCache struct{}
 
 func (e *LdapStoreErrorCache) Error() string {
 	return "A general cache error was encountered"
 }
 
+// LdapStoreErrorCacheUpdate is an error relating to an error updating the local cache
 type LdapStoreErrorCacheUpdate struct{}
 
 func (e *LdapStoreErrorCacheUpdate) Error() string {
 	return "The cache update operation failed"
 }
 
+// LdapStoreErrorCacheFetch is an error relating to fetching an item from cache
 type LdapStoreErrorCacheFetch struct{}
 
 func (e *LdapStoreErrorCacheFetch) Error() string {
